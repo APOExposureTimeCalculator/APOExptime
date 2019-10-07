c47abdabd2540b56865bdd127256d5e01ca0b621
killerrabbit3 <stonemaa@nmsu.edu>
2019-10-07 14:57:54 -0600

    Update APOexptime.py

--- APOexptime/APOexptime.py
+++ APOexptime/APOexptime.py
@@ -8,4 +8,3 @@ Created on Mon Oct  7 14:25:10 2019
 class Instrument:
     def __init__(self, Instr_name, Instr_transmission, Instr_efficiency,
                  readout_noise, filter_wavelengths):
-      
