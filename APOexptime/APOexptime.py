bf619fd52278da810ba8ce17d23042e13dd47cdf
Alexander Stone-Martinez <52977017+killerrabbit3@users.noreply.github.com>
2019-10-07 15:00:29 -0600

    update

--- APOexptime/APOexptime.py
+++ APOexptime/APOexptime.py
@@ -5,9 +5,6 @@ Created on Mon Oct  7 14:25:10 2019
 @authors: Alexander
 """
 
-<<<<<<< HEAD
 class Instrument:
     def __init__(self, Instr_name, Instr_transmission, Instr_efficiency,
                  readout_noise, filter_wavelengths):
-=======
->>>>>>> parent of b93cbea... update
