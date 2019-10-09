f06f737a11fbf132922047477b12c513078e705b
killerrabbit3 <stonemaa@nmsu.edu>
2019-10-09 15:43:38 -0600

    Added to Instrument class
    added for loop so that it checks for filter profiles in Instrument directory and loads them in

--- APOexptime/APOexptime.py
+++ APOexptime/APOexptime.py
@@ -5,10 +5,21 @@ Created on Mon Oct  7 14:25:10 2019
 @authors: Alexander
 """
 
+from astropy.io import ascii
+import glob
+
 class Instrument:
     def __init__(self, Instr_name):
         
+        para = ascii.read(Instr_name +"/"+Instr_name + '_param.data')
         
+        efficiency = ascii.read(Instr_name +"/"+Instr_name + '_eff.data')
+        
+        f = glob.glob(Instr_name+ '/' + '*filter.data')
+        for i, row in enumerate(f):
+            filt = ascii.read(row)
+            name = row[row.find('filter')-1]+'filter'
+            setattr(Instrument, name, filt)
         
         self.transmission = 
         self.efficiency = 
@@ -16,7 +27,7 @@ class Instrument:
         self.filter_wavelengths =
 
 # How to read our data files:
-from astropy.io import ascii
+
 data = ascii.read("test.data")
 values = data["col1"]
 
