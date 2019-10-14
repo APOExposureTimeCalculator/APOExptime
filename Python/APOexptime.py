c4668dd5045e97ea148b10c6a50b111480d55d9b
killerrabbit3 <stonemaa@nmsu.edu>
2019-10-14 15:50:53 -0600

    Finished Instrument Class *
    * first iteration

--- Python/APOexptime.py
+++ Python/APOexptime.py
@@ -13,29 +13,37 @@ class Instrument:
         
         para = ascii.read('data/apo3_5m/'+Instr_name +"/"+Instr_name + '_param.data')
         
-        efficiency = ascii.read('data/apo3_5m/'+Instr_name +"/"+Instr_name + '_eff.data')
+        efficiency = ascii.read('data/apo3_5m/'+Instr_name +"/"+Instr_name + '_qe.data')
         
       
         for row in para['Filters']:
             filt = ascii.read('data/apo3_5m/'+Instr_name +"/"+row)
-            name = row.split('.data')[0]
-            setattr(Instrument, name, filt)
+            data_name = row.split('.dat')[0]
+            filt_wavelength=filt["col1"]
+            filt_throughput=filt["col2"]
+            filt_interpolated = interpolate.InterpolatedUnivariateSpline(
+            filt_wavelength, filt_throughput)
+            setattr(Instrument, data_name, filt_interpolated)
+            
+            filt_range = [filt["col1"][0], filt["col1"][len(filt["col1"])-1]]
+            range_name = row.split('filter.dat')[0]+'range'
+            setattr(Instrument, range_name, filt_range)
         
-        efficiency_wavelength=efficiency["col1"]
-        efficiency_percent=efficiency["col2"]/100  #divided by 100 to turn into decimal
-        filt_wavelength=f["col1"]
-        filt_throughput=f["col2"]
-        
-        efficiency_interpolated = interpolate.InterpolatedUnivariateSpline(
-                efficiency_wavelength, efficiency_percent)
-        filt_interpolated = interpolate.InterpolatedUnivariateSpline(
-                filt_wavelength, filt_throughput)
+        qefficiency_wavelength=efficiency["col1"]
+        qefficiency_percent=efficiency["col2"]/100  #divided by 100 to turn into decimal
 
         
-        self.transmission = 
+        efficiency_interpolated = interpolate.InterpolatedUnivariateSpline(
+                qefficiency_wavelength, qefficiency_percent)
+ 
+        if para['isImager'][0] == 0:
+            self.dispersion = para['Dispersion']
+        
+        
         self.efficiency = efficiency_interpolated
-        self.readout_noise = 
-        self.filter_wavelengths = filt_interpolated
+        self.readout_noise = para['readoutnoise[electrons]'][0]
+        self.filter_num = para['FilterNum']
+        self.gain = para['gain']
 
 # How to read our data files:
 
