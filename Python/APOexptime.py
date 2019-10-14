438f4bb43bc3a231f2af28c1921ea80b62f4f5f0
killerrabbit3 <stonemaa@nmsu.edu>
2019-10-14 14:37:33 -0600

    Changed file stuff

--- Python/APOexptime.py
+++ Python/APOexptime.py
@@ -6,7 +6,6 @@ Created on Mon Oct  7 14:25:10 2019
 """
 
 from astropy.io import ascii
-import glob
 from scipy import interpolate
 
 class Instrument:
@@ -16,10 +15,10 @@ class Instrument:
         
         efficiency = ascii.read('data/apo3_5m/'+Instr_name +"/"+Instr_name + '_eff.data')
         
-        f = glob.glob('data/apo3_5m/'+Instr_name+ '/' + '*filter.data')
-        for i in len(int(para['FilterNum'])):
-            filt = ascii.read(row)
-            name = row[row.find('filter')-1]+'filter'
+      
+        for row in para['Filters']:
+            filt = ascii.read('data/apo3_5m/'+Instr_name +"/"+row)
+            name = row.split('.data')[0]
             setattr(Instrument, name, filt)
         
         efficiency_wavelength=efficiency["col1"]
@@ -40,10 +39,4 @@ class Instrument:
 
 # How to read our data files:
 
-data = ascii.read("test.data")
-values = data["col1"]
 
-rogers_mom = values[0]
-hasan = values[1]
-
-print(rogers_mom, hasan)
