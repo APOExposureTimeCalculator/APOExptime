4786c80bc1b40b6cbe7eed2104455be907582aae
Alexander (Desktop) <stonemaa@nmsu.edu>
2019-11-14 20:10:12 -0700

    Added Spectra support

--- Python/APOinputclasses.py
+++ Python/APOinputclasses.py
@@ -118,12 +118,14 @@ class Observation:
         :param telescope:
         """
         # telescope_transm = telescope.transmission
+        self.detector_qe = instrument.efficiency
         self.telescope_area = (175 ** 2) * np.pi
         self.source = target.F_lambda
         self.skySED = sky.sky_emission
         self.skyTransmission = sky.sky_transmission
         self.seeing = sky.seeing
         self.rdnoise = instrument.readout_noise
+        self.isImager = instrument.isImager
 
         self.counts(self.source, instrument)
         self.skycounts(self.skySED, instrument)
@@ -135,41 +137,63 @@ class Observation:
         :param instrument:
         :return:
         """
-        Npix = np.pi * ((self.seeing / 2) ** 2) / (instrument.scale ** 2)
+        if self.isImager == 1:
+            Npix = np.pi * ((self.seeing / 2) ** 2) / (instrument.scale ** 2)
+        else:
+            Npix = instrument.Npix
         return Npix
 
     def skycounts(self, sky, instrument):
         att = dir(instrument)
-        self.detector_qe = instrument.efficiency
-        for row in att:
-            if row.find('filter') > 0:
-                filter_profile = getattr(instrument, row)
-                integrate_range = getattr(instrument, row.replace('filter', 'range'))
-                interpolationrange = range(integrate_range[0], integrate_range[1])
-                s_integrade = sky_integradeInterpolate([sky, self.detector_qe, self.skyTransmission, filter_profile],
-                                                       interpolationrange)
-                s_prime = self.telescope_area * ((self.seeing / 2) ** 2) * s_integrade.integral(integrate_range[0],
-                                                                                                integrate_range[1])
-                count_name = row.replace('_filter', '') + '_skycountrate'
-                setattr(Observation, count_name, s_prime)
+        if self.isImager == 1:
+            for row in att:
+                if row.find('filter') > 0:
+                    filter_profile = getattr(instrument, row)
+                    integrate_range = getattr(instrument, row.replace('filter', 'range'))
+                    interpolationrange = range(integrate_range[0], integrate_range[1])
+                    s_integrade = s_integradeInterpolate([sky, self.detector_qe, self.skyTransmission, filter_profile],
+                                                         interpolationrange)
+                    s_prime_dlam = [self.telescope_area * ((self.seeing / 2) ** 2) * s_integrade[1], s_integrade[0]]
+                    s_prime = np.trapz(s_prime_dlam[0], s_prime_dlam[1])
+                    count_name = row.replace('_filter', '') + '_skycountrate'
+                    setattr(Observation, count_name, s_prime)
+        else:
+            interpolationrange = range(instrument.range[0], instrument.range[1])
+            h = 6.626 * 10 ** (-27)  # ergs*s
+            c = 2.9979 * 10 ** (18)  # A/s
+            s_integrade = s_integradeInterpolate([sky, self.detector_qe, self.skyTransmission],
+                                                 interpolationrange)
+
+            self.sky_prime_dlam = [(self.telescope_area * (1 / (h * c)) * s_integrade[1]), s_integrade[0]]
 
     def counts(self, source, instrument):
         att = dir(instrument)
-        self.detector_qe = instrument.efficiency
-        for row in att:
-            if row.find('filter') > 0:
-                filter_profile = getattr(instrument, row)
-                integrate_range = getattr(instrument, row.replace('filter', 'range'))
-                interpolationrange = range(integrate_range[0], integrate_range[1])
-                h = 6.626 * 10 ** (-27)  # ergs*s
-                c = 2.9979 * 10 ** (18)  # A/s
-                s_integrade = s_integradeInterpolate([source, self.detector_qe, self.skyTransmission, filter_profile],
-                                                     interpolationrange)
+        if self.isImager == 1:
+            for row in att:
+                if row.find('filter') > 0:
+                    filter_profile = getattr(instrument, row)
+                    integrate_range = getattr(instrument, row.replace('filter', 'range'))
+                    interpolationrange = range(integrate_range[0], integrate_range[1])
+                    h = 6.626 * 10 ** (-27)  # ergs*s
+                    c = 2.9979 * 10 ** (18)  # A/s
+                    s_integrade = s_integradeInterpolate(
+                        [source, self.detector_qe, self.skyTransmission, filter_profile],
+                        interpolationrange)
 
-                s_prime_dlam = [(self.telescope_area * (1 / (h * c)) * s_integrade[1]), s_integrade[0]]
-                s_prime = np.trapz(s_prime_dlam[0], s_prime_dlam[1])
-                count_name = row.replace('_filter', '') + '_sourcecountrate'
-                setattr(Observation, count_name, s_prime)
+                    s_prime_dlam = [(self.telescope_area * (1 / (h * c)) * s_integrade[1] * interpolationrange),
+                                    s_integrade[0]]
+                    s_prime = np.trapz(s_prime_dlam[0], s_prime_dlam[1])
+                    count_name = row.replace('_filter', '') + '_sourcecountrate'
+                    setattr(Observation, count_name, s_prime)
+        else:
+            interpolationrange = range(instrument.range[0], instrument.range[1])
+            h = 6.626 * 10 ** (-27)  # ergs*s
+            c = 2.9979 * 10 ** (18)  # A/s
+            s_integrade = s_integradeInterpolate([source, self.detector_qe, self.skyTransmission],
+                                                 interpolationrange)
+
+            self.s_prime_dlam = [(self.telescope_area * (1 / (h * c)) * s_integrade[1] * interpolationrange),
+                                         s_integrade[0]]
 
     def SNfromTime(self, exptime):
         """
@@ -179,35 +203,49 @@ class Observation:
         """
         att = dir(self)
         returnList = []
-        for row in att:
-            if row.find('sourcecountrate') > 0:
-                sourceCount = getattr(self, row)
-                skyCount = getattr(self, row.replace('source', 'sky'))
+        if self.isImager == 1:
+            for row in att:
+                if row.find('sourcecountrate') > 0:
+                    sourceCount = getattr(self, row)
+                    skyCount = getattr(self, row.replace('source', 'sky'))
 
-                SN = (sourceCount * exptime) / np.sqrt(sourceCount * exptime + skyCount * exptime
-                                                       + self.Npix * self.rdnoise ** 2)
-                SNname = row.replace('sourcecountrate', 'SN')
-                returnList.append([SN, SNname])
+                    SN = (sourceCount * exptime) / np.sqrt(sourceCount * exptime + skyCount * exptime
+                                                           + self.Npix * self.rdnoise ** 2)
+                    SNname = row.replace('sourcecountrate', 'SN')
+                    returnList.append([SN, SNname])
 
-                setattr(Observation, SNname, SN)
+                    setattr(Observation, SNname, SN)
+
+        else:
+            SN_d_lam = (self.s_prime_dlam[0] * exptime) / np.sqrt(
+                self.s_prime_dlam[0] * exptime + self.sky_prime_dlam[0] * exptime
+                + (self.Npix * self.rdnoise ** 2))
+            returnList = [np.array(self.s_prime_dlam[1]), SN_d_lam]
+
         return returnList
         # PLOT SHIT HERE
 
     def TimefromSN(self, SN):
         att = dir(self)
         returnList = []
-        for row in att:
-            if row.find('sourcecountrate') > 0:
-                sourceCount = getattr(self, row)
-                skyCount = getattr(self, row.replace('source', 'sky'))
+        if self.isImager == 1:
+            for row in att:
+                if row.find('sourcecountrate') > 0:
+                    sourceCount = getattr(self, row)
+                    skyCount = getattr(self, row.replace('source', 'sky'))
 
-                t = (1. / (2. * sourceCount ** 2)) * (SN ** 2 * (sourceCount + skyCount) + np.sqrt(SN ** 4 * (
-                        sourceCount + skyCount) ** 2 + 4. * self.Npix * (sourceCount * SN * self.rdnoise) ** 2))
+                    t = (1. / (2. * sourceCount ** 2)) * (SN ** 2 * (sourceCount + skyCount) + np.sqrt(SN ** 4 * (
+                            sourceCount + skyCount) ** 2 + 4. * self.Npix * (sourceCount * SN * self.rdnoise) ** 2))
 
-                Tname = row.replace('sourcecountrate', 'time')
-                returnList.append([t, Tname])
+                    Tname = row.replace('sourcecountrate', 'time')
+                    returnList.append([t, Tname])
 
-                setattr(Observation, Tname, t)
+                    setattr(Observation, Tname, t)
+        else:
+            t_d_lam = (1. / (2. * self.s_prime_dlam[0] ** 2)) * (SN ** 2 * (self.s_prime_dlam[0] + self.sky_prime_dlam[0]) + np.sqrt(SN ** 4 * (
+                            self.s_prime_dlam[0] + self.sky_prime_dlam[0]) ** 2 + 4. * self.Npix * (self.s_prime_dlam[0] * SN * self.rdnoise) ** 2))
+            returnList = [np.array(self.s_prime_dlam[1]), t_d_lam]
+
         return returnList
     #         # PLOT SHIT HERE
 
@@ -234,19 +272,19 @@ class Instrument:
         para = ascii.read('../data/apo3_5m/' + Instr_name + "/" + Instr_name + '_param.data')
 
         efficiency = ascii.read('../data/apo3_5m/' + Instr_name + "/" + Instr_name + '_qe.data')
+        if para['isImager'][0] > 0:
+            for row in para['Filters']:
+                filt = ascii.read('../data/apo3_5m/' + Instr_name + "/" + row)
+                data_name = row.split('.dat')[0]
+                filt_wavelength = filt["col1"]
+                filt_throughput = filt["col2"]
+                filt_interpolated = interpolate.InterpolatedUnivariateSpline(
+                    filt_wavelength, filt_throughput)
+                setattr(Instrument, data_name, filt_interpolated)
 
-        for row in para['Filters']:
-            filt = ascii.read('../data/apo3_5m/' + Instr_name + "/" + row)
-            data_name = row.split('.dat')[0]
-            filt_wavelength = filt["col1"]
-            filt_throughput = filt["col2"]
-            filt_interpolated = interpolate.InterpolatedUnivariateSpline(
-                filt_wavelength, filt_throughput)
-            setattr(Instrument, data_name, filt_interpolated)
-
-            filt_range = [filt["col1"][0], filt["col1"][len(filt["col1"]) - 1]]
-            range_name = row.split('filter.dat')[0] + 'range'
-            setattr(Instrument, range_name, filt_range)
+                filt_range = [filt["col1"][0], filt["col1"][len(filt["col1"]) - 1]]
+                range_name = row.split('filter.dat')[0] + 'range'
+                setattr(Instrument, range_name, filt_range)
 
         qefficiency_wavelength = efficiency["col1"] * 10  # multiplied by 10 to turn to angstroms
         qefficiency_percent = efficiency["col2"] / 100  # divided by 100 to turn into decimal
@@ -256,12 +294,15 @@ class Instrument:
 
         if para['isImager'][0] == 0:
             self.dispersion = para['Dispersion']
+            self.range = [para['rangeMin'][0], para['rangeMax'][0]]
+            self.Npix = para['Npix'][0]
 
         self.efficiency = efficiency_interpolated
         self.readout_noise = para['readoutnoise[electrons]'][0]
         self.filter_num = para['FilterNum'][0]
         self.gain = para['gain'][0]
         self.scale = para['plate_scale[arcsec/pix]'][0]
+        self.isImager = para['isImager'][0]
 
 
 def s_integradeInterpolate(functions, interpolation_range):
@@ -278,16 +319,5 @@ def s_integradeInterpolate(functions, interpolation_range):
             x = np.ones(len(interpolation_range))
         x = f(interpolation_range) * x
 
-    return [interpolation_range, (x * interpolation_range)]
+    return [interpolation_range, x]
 
-
-def sky_integradeInterpolate(functions, interpolation_range):
-    """Takes multiple interpolation objects and multiplies them together then reinterpolates to output a single
-    interpolation object :param functions: :param interpolation_range: :return:
-    """
-    for i, f in enumerate(functions):
-        if i == 0:
-            x = np.ones(len(interpolation_range))
-        x = f(interpolation_range) * x
-
-    return interpolate.InterpolatedUnivariateSpline(interpolation_range, x)
--- Python/Tester.py
+++ Python/Tester.py
@@ -2,15 +2,20 @@
 
 # execfile('APOinputclasses.py')
 from Python.APOinputclasses import Sky, Target, Instrument, Observation
+import matplotlib.pyplot as plt
 
 sky = Sky(lunar_phase=0)
 star = Target(20, 'VEGAMAG', [5000, 7000], temp=6000)
 inst = Instrument('Arctic')
 ob = Observation(star, sky, inst)
 
-times = ob.TimefromSN(100)
+times = ob.TimefromSN(10)
 
-sn = ob.SNfromTime(100)
+sn = ob.SNfromTime(1000)
 
-print(times)
+
+#plt.plot(sn[0], sn[1])
+#plt.plot(times[0], times[1])
+
+
 print(sn)
