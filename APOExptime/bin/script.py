3e8e652dcf1abd290c9eb188337c2c262aea66e3
canasmh <55603689+canasmh@users.noreply.github.com>
2019-11-15 20:23:46 -0700

    Added scripts.py to bin directory

---
+++ APOExptime/bin/script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python                                                           
+
+"""                                                                             
+Just following instructions from                                                
+https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html#setup-\
+py-exercise-small-example-package                                               
+"""
+
+import sys,os
+from APOExptime import inputs
+
+
+if __name__=="__main__":
+     main.main()
