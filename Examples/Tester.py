aead0a95c369e23b03c27c70c5db0e764aa18b94
alexander <stonemaa@nmsu.edu>
2020-02-24 15:16:15 -0700

    Rearranged directory for packaging

---
+++ Examples/Tester.py
@@ -0,0 +1,27 @@
+"""Script to test APO Exposure timer calc"""
+
+# execfile('APOinputclasses.py')
+from Python.APOinputclasses import Sky, Target, Instrument, Observation
+from Python.Plotter import makeplots
+
+sky = Sky(lunar_phase=0, seeing=1)
+star = Target(19, 'VEGAMAG', [5000, 6000], temp=6000)
+inst1 = Instrument('Arctic')
+
+
+ob1 = Observation(star, sky, inst1)
+
+
+sn1 = ob1.SNfromTime(200)
+print(sn1)
+
+t1 = ob1.TimefromSN(50)
+
+#plt.plot(t1[0], t1[1])
+# plt.plot(times[0], times[1])
+
+#plt.plot(sn1[2][0], sn1[2][1])
+# plt.plot(times[0], times[1])
+
+
+#makeplots(ob1, 'Time')
